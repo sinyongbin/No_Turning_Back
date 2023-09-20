e3c621a0c8371cf10b1cd51b43f517dc73f07f07
jae0014 <jaedafeeder@gmail.com>
2023-09-20 10:44:29 +0900

    --MyModal.tsx: modal for login --typeModlles.ts holds all types --route testing for routing --globals.css deleted default value and replaced with custuom

---
+++ src/app/api/posting/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse , NextRequest } from "next/server";
+
+export async function GET() {
+
+    return null;
+}
+export async function POST(req:NextRequest) {
+
+    return new Response("OK")
+}
+export async function PUT()
+{
+    return null;
+}
+export async function DELETE()
+{
+    return null;
+}
