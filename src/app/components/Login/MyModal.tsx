606247b7b8a3d2d4cd37fff702bc78a06ede57f5
jae0014 <jaedafeeder@gmail.com>
2023-09-20 11:26:53 +0900

    Login modal window typeModules: holds all types for this project

---
+++ src/app/components/Login/MyModal.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import {modalControl}from '@/typeModules'
+import  Login  from './Login'
+
+export function MyModal({ closeModal , isOpen} : modalControl) {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-center text-gray-900"
+                  >
+                    JdoBak
+                  </Dialog.Title>
+                  <div className="mt-2">
+                      <Login/>
+                  </div>
+                  <hr className='text-gray-800'/>
+                  <div className="mt-4 text-gray-900">
+                       SSN Login
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
---
+++ src/typeModules.ts
@@ -0,0 +1,6 @@
+export type modalControl = 
+{
+   closeModal: any
+   isOpen : boolean
+}
+
