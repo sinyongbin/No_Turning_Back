2ce3592a065685fa6481e0a93f852aecca4b32d1
kcs <kcsft920426@gmail.com>
2023-09-27 17:27:21 +0900

    first setting

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,17 +1,15 @@
 import { NextResponse , NextRequest } from "next/server";
 import prisma from "@/db";
+import {Gender as gender} from 'prisma/prisma-client'
+
 import axios from "axios";
 export async function GET() {
-    const data = await prisma.test.findMany()
-    const url = ""
-    let sql = await axios.get(url).then(e=>{
-        console.log(e)
-    })
+    const profile = {
+        bio : gender.MALE,
+        email: "jadedafeeeder@gmail.com",
+        nickname: "nicknametest",
+    }
+    const data = await prisma.profile.create({data:profile })
+
     return NextResponse.json(data)
-}
-export async function POST(req:NextRequest) {
-    const data =  await req.formData()
-    let {name, address, address2,phone,email} = Object.fromEntries(data);
-    let body = Object.fromEntries(data);
-    return new Response("OK")
 }
