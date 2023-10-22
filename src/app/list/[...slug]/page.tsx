d33ccc5cdde2f0333b88b8ff8c691c7b12de7152
hyunndong0203 <hyunndong@nate.com>
2023-10-22 18:08:06 +0900

    푸쉬 위한커밋

--- src/app/list/[...slug]/page.tsx
+++ src/app/list/[...slug]/page.tsx
@@ -50,44 +50,51 @@ export default function Page(query:{params : any}) {
         setModalOpen(false);
       };
     
-    
-      return (
-        
+  function ListDetailPage(){
+      return(
         <div className="bg-white">
-          <div><Nav/></div>
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-            <h2 className="sr-only">Products</h2>
-    
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {visibleProducts.map((e: any, key: number) => (
-                <div key={key} className="product-item">
-                  <a href={`/listdetail/${e.id}`} className="group">
-                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                      <img
-                        src={e.images[0]}
-                        alt={e.imageAlt} // alt 속성 추가
-                        className="h-full w-full object-cover object-center group-hover:opacity-75"
-                        style={{ width: '280px', height: '280px' }}
-                      />
-                    </div>
-                    <h3 className="mt-4 text-sm text-gray-700">{e.title}</h3>
-                    <p className="mt-1 text-lg font-medium text-gray-900">{e.starting_price}</p>
-                  </a>
-                </div>
-              ))}
-            </div>
-            <div className="text-center mt-4">
-              {visibleProducts.length < products.length && (
-                <button
-                  className="bg-white hover:bg-gray-300 text-black font-bold py-2 px-4 border-2 rounded-lg animate-bounce"
-                  onClick={loadMoreProducts}
-                >
-                  더 보기
-                </button>
-              )}
-            </div>
+        <div><Nav/></div>
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+  
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {visibleProducts.map((e: any, key: number) => (
+              <div key={key} className="product-item">
+                <a href={`/listdetail/${e.id}`} className="group">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                    <img
+                      src={e.images[0]}
+                      alt={e.imageAlt} // alt 속성 추가
+                      className="h-full w-full object-cover object-center group-hover:opacity-75"
+                      style={{ width: '280px', height: '280px' }}
+                    />
+                  </div>
+                  <h3 className="mt-4 text-sm text-gray-700">{e.title}</h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900">{e.starting_price}</p>
+                </a>
+              </div>
+            ))}
+          </div>
+          <div className="text-center mt-4">
+            {visibleProducts.length < products.length && (
+              <button
+                className="bg-white hover:bg-gray-300 text-black font-bold py-2 px-4 border-2 rounded-lg animate-bounce"
+                onClick={loadMoreProducts}
+              >
+                더 보기
+              </button>
+            )}
           </div>
         </div>
-      );
-    }
-    
+      </div>
+    );
+  }
+  
+  return (
+        <>
+            {postData == undefined ? <div className="text-center text-5xl"> 데이터 로딩중입니다</div> : <ListDetailPage/>}
+        </>
+      )
+  }
+        
+       
--- src/app/listdetail/[id]/page.tsx
+++ src/app/listdetail/[id]/page.tsx
@@ -2,8 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 import ImageViewer from '../_components/detailImg';
-import DetailModal from '@/app/message/_components/modal';
 import Bidding from '@/app/transaction/components/bidding';
+import SendModal from '@/app/message/_components/sendmodal';
 
 export default function Detail({ params }: { params: { id: string } }) {
   const [newComment, setNewComment] = useState(''); // Comment 타입 사용
@@ -11,7 +11,16 @@ export default function Detail({ params }: { params: { id: string } }) {
   const [isOpen, setIsOpen] = useState(false);
   const [postData, setPostData] = useState<any>({});
   const [nick, setNick] = useState<string>("");
+  const [isModal2Open, setIsModal2Open] = useState(false);
 
+  console.log("isModal2Open 상태:", isModal2Open);
+
+  const openModal2 = () => {  
+    setIsModal2Open(true);
+  };
+  const closeModal2 = () => {
+    setIsModal2Open(false);
+  };
   const id = params.id;
 
   useEffect(() => {
@@ -39,13 +48,16 @@ export default function Detail({ params }: { params: { id: string } }) {
 
   const openModal = () => {
     setModalOpen(true);
+   
   };
-
   const closeModal = () => {
-
     setIsOpen(false);
   };
 
+  const handleOpenModal = () => {
+    openModal2();
+  };
+  
   function Main() {
     return (
       <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
@@ -92,12 +104,13 @@ export default function Detail({ params }: { params: { id: string } }) {
                   <div className="flex justify-center mt-4">
                     <button
                       className="w-[500px] border-2 bg-white text-black px-4 py-4 rounded-lg hover:bg-zinc-300"
-                      onClick={openModal}
-                    >
+                      onClick={handleOpenModal}>
                       문의하기
                     </button>
-                    {isModalOpen && <></>/*<DetailModal id={id} isOpen={} closeModal={} nickname={nick} />*/}   
-                  </div>
+                    {isModalOpen && (
+                      <SendModal id={id} isModal2Open={isModal2Open} closeModal2={closeModal2} nickname={nick} />
+                    )}
+                    </div>
                 </div>
                 <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">입찰시 주의사항</h2>
                 <p className="mt-6">약관의 동의하시오</p>
@@ -127,7 +140,7 @@ export default function Detail({ params }: { params: { id: string } }) {
 
   return (
     <>
-      {postData.post == undefined ? <div></div> : <Main />}
+      {postData.post == undefined ? <div className='text-center text-5xl' > 데이터를 불러오는중입니다 잠시만 기달려주세요</div> : <Main />}
     </>
   );
 }
