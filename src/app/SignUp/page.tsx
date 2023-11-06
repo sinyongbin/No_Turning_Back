22dfbf987c2b24a61c1375fd86c9d56279d668d0
JL2101 <sauos12345@gmail.com>
2023-11-06 11:28:08 +0900

    회원가입 수정

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -63,11 +63,15 @@ export default function Signup() {
           body: new FormData(e.currentTarget),
         })
         .then((response) => { 
-          if (response.status !== 200) {
-            throw new Error('서버 응답이 실패했습니다. 상태 코드: ' + response.status);
-          } else {
+          if (response.status === 200) {
             alert(`회원가입을 축하합니다! ${nickName} 님`);
             location.href='./';
+          } else if (response.status === 404){
+            alert(`이미 존재하는 아이디입니다.`);
+            return;
+          } else {
+            alert(`새로고침한 다음에 다시 시도해주세요!`);
+            return;
           }
         })
           .catch((error) => {
